fe6e8a56554e7b42d2a01d34dfe9e124c99ce80f
Radu Bortan <72158314+radubortan@users.noreply.github.com>
2022-11-04 15:00:11 +0100

    finished confirmation email with gmail

--- src/main/java/fr/polytech/bbr/fsj/FsjApplication.java
+++ src/main/java/fr/polytech/bbr/fsj/FsjApplication.java
@@ -1,11 +1,13 @@
 package fr.polytech.bbr.fsj;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class FsjApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FsjApplication.class, args);
 	}
---
+++ src/main/java/fr/polytech/bbr/fsj/email/EmailSender.java
@@ -0,0 +1,5 @@
+package fr.polytech.bbr.fsj.email;
+
+public interface EmailSender {
+    void send(String to, String email);
+}
