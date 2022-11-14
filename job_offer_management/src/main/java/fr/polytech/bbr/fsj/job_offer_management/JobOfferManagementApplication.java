4f62fae2287f357393eaffe87c6869e87c56ece5
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-11-14 18:27:52 +0100

    init job_offer_management

---
+++ job_offer_management/src/main/java/fr/polytech/bbr/fsj/job_offer_management/JobOfferManagementApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.bbr.fsj.job_offer_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobOfferManagementApplication.class, args);
+    }
+
+}
---
+++ job_offer_management/src/test/java/fr/polytech/bbr/fsj/job_offer_management/JobOfferManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.bbr.fsj.job_offer_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
