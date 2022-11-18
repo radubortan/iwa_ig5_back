cb3d15b72cff5e6edf3cfbaaf79049870fd13e0d
Vincent-Baret1 <vincent.baret@etu.umontpellier.fr>
2022-11-18 17:54:10 +0100

    feat(): add view CV functionnality

--- cv_management/src/main/java/fr/polytech/bbr/fsj/controllers/CvController.java
+++ cv_management/src/main/java/fr/polytech/bbr/fsj/controllers/CvController.java
@@ -3,8 +3,11 @@ package fr.polytech.bbr.fsj.controllers;
 
 import fr.polytech.bbr.fsj.FileUploader;
 
-import okhttp3.Response;
+import io.minio.StatObjectResponse;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -16,6 +19,8 @@ import java.nio.file.Path;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
 
 @RestController
 public class CvController {
@@ -23,8 +28,7 @@ public class CvController {
 
     @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/cv-upload")
-    @ResponseStatus(HttpStatus.OK)
-    public String uploadCV(@RequestParam("file") MultipartFile file) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
+    public ResponseEntity<String> uploadCV(@RequestParam("file") MultipartFile file) throws IOException, NoSuchAlgorithmException, InvalidKeyException {
         InputStream inputStream = file.getInputStream();
         FileUploader fileUploader = new FileUploader();
         Path path = Path.of(file.getOriginalFilename());
@@ -33,9 +37,29 @@ public class CvController {
 
         boolean status = fileUploader.uploadFile(path.toString(), type, size, inputStream);
         if(status == true) {
-            return "successfully upload file";
+            return ResponseEntity.ok().body("File successfully uploaded");
         } else {
-            return "an error occured";
+            return ResponseEntity.status(BAD_REQUEST).body(null);
         }
     }
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping("/cv-view")
+    public ResponseEntity<InputStreamResource> viewCV(@RequestParam("fileName") String fileName) {
+        FileUploader fileUploader = new FileUploader();
+        InputStream stream = fileUploader.getFile(fileName);
+        StatObjectResponse stat = fileUploader.getFileMetadata(fileName);
+        InputStreamResource inputStreamResource = new InputStreamResource(stream);
+        if(stream != null) {
+            return ResponseEntity
+                    .ok()
+                    .contentType(MediaType.APPLICATION_PDF)
+                    .contentLength(stat.size())
+                    .header("Content-disposition", "attachment; filename=" + fileName)
+                    .body(inputStreamResource);
+        } else {
+            return ResponseEntity.status(BAD_REQUEST).body(null);
+        }
+    }
+
 }
