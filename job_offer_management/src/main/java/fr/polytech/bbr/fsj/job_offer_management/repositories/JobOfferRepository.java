62d321059817a91023c839242f8bfba0e15cb882
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-11-18 14:31:52 +0100

    get job offers by employer id feature

--- job_offer_management/src/main/java/fr/polytech/bbr/fsj/job_offer_management/repositories/JobOfferRepository.java
+++ job_offer_management/src/main/java/fr/polytech/bbr/fsj/job_offer_management/repositories/JobOfferRepository.java
@@ -2,8 +2,16 @@ package fr.polytech.bbr.fsj.job_offer_management.repositories;
 
 import fr.polytech.bbr.fsj.job_offer_management.models.JobOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+
 @Repository
-public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {}
+public interface JobOfferRepository extends JpaRepository<JobOffer, Long> {
+    @Query(
+            value = "SELECT * FROM job_offer j WHERE j.id_employer = ?1",
+            nativeQuery = true)
+    Collection<JobOffer> findJobOffersByIdEmployer(Long idEmployer);
+}
 
