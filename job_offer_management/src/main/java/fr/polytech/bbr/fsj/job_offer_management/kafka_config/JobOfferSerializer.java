59a49b2853de696b58f76a33555ae94dc33d186c
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-11-21 11:11:16 +0100

    Job Offer Kafka Producer

---
+++ job_offer_management/src/main/java/fr/polytech/bbr/fsj/job_offer_management/kafka_config/JobOfferSerializer.java
@@ -0,0 +1,34 @@
+package fr.polytech.bbr.fsj.job_offer_management.kafka_config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fr.polytech.bbr.fsj.job_offer_management.models.JobOffer;
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.util.Map;
+
+public class JobOfferSerializer implements Serializer<JobOffer> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void configure(Map<String, ?> configs, boolean isKey) {
+    }
+    @Override
+    public byte[] serialize(String topic, JobOffer data) {
+        try {
+            if (data == null){
+                System.out.println("Null received at serializing");
+                return null;
+            }
+            System.out.println("Serializing...");
+            return objectMapper.writeValueAsBytes(data);
+        } catch (Exception e) {
+            throw new SerializationException("Error when serializing MessageDto to byte[]");
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+
+}
