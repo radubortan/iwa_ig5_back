e7a6db800d64657f3d1913274b41200a0cdda0ea
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-11-21 12:25:49 +0100

    recommendation management initialization; test-workflow initialization

---
+++ recommendation_management/src/test/java/fr/polytech/brr/fsj/recommendation_management/RecommendationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.brr.fsj.recommendation_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
